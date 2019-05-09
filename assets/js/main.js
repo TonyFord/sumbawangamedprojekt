ee86b8a8adb47fb8f8dfb77d8061b88493b097f4
Sebastian Gampe <mail2@strategy-investor.de>
2019-05-09 13:38:52 +0200

    Update main.js
    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,8 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(function() {
+  
+  /*
   $("section h1, section h2, section h3").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
@@ -26,6 +28,7 @@ $(function() {
     $(this).parent().addClass("active");
     event.preventDefault();
   });
+  */
 
   sectionHeight();
 
